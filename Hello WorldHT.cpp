81d77bd0f15860948c6f1966c2fbc4cfab8383da
Sielim <48602792+SielimDDk@users.noreply.github.com>
2019-03-19 15:24:05 +0200

    Add files via upload

---
+++ Hello WorldHT.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	printf("hello world\n");
+	cout << "HELLO WORLD" << endl;
+
+	_getch();
+	return 0;
+}
